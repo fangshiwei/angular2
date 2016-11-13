818648835efe96844b04dc1c8a6a2ecc2a82738d
Fang Shiwei <fangshiwei@outlook.com>
2016-11-13 23:50:52 +0800

    add simple http code

--- http/src/app/app.module.ts
+++ http/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent
   ],
   imports: [
     BrowserModule,
